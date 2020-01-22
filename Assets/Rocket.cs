2259f556573f922810882167ba0ec40c4d14a0af
inann <inann231@gmail.com>
2020-01-21 22:23:01 -0800

    Add Rocket Script

---
+++ Assets/Rocket.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+      rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      ProcessInput();
+    }
+
+    private void ProcessInput() {
+      if (Input.GetKey(KeyCode.Space)) {
+        print("Thrust");
+        rigidbody.AddRelativeForce(Vector3.up);
+      }
+      if (Input.GetKey(KeyCode.A)) {
+        if (!Input.GetKey(KeyCode.D)) {
+          print("Rot Left");
+        }
+      }
+      else if (Input.GetKey(KeyCode.D)){
+        if (!Input.GetKey(KeyCode.A)) {
+          print("Rot Right");
+        }
+      }
+    }
+  }
