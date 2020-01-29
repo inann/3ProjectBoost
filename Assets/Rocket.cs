4e3e2d2b5c9bb4159ebd38d3be1829016fbf86ef
inann <inann231@gmail.com>
2020-01-28 23:49:37 -0800

    Adding Audio

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidbody;
+    float rotationspeed = 45.0f;
+    float fadeDuration = 0.75f;
+    AudioSource audiosource;
     // Start is called before the first frame update
     void Start()
     {
       rigidbody = GetComponent<Rigidbody>();
+      audiosource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -18,19 +22,41 @@ public class Rocket : MonoBehaviour
     }
 
     private void ProcessInput() {
+      if (Input.GetKeyDown(KeyCode.Space)) {
+        audiosource.Play();
+      }
+
+      if (Input.GetKeyUp(KeyCode.Space)) {
+        IEnumerator fd = FadeOut(audiosource, fadeDuration);
+        StartCoroutine(fd);
+      }
+
       if (Input.GetKey(KeyCode.Space)) {
         print("Thrust");
         rigidbody.AddRelativeForce(Vector3.up);
       }
       if (Input.GetKey(KeyCode.A)) {
         if (!Input.GetKey(KeyCode.D)) {
-          print("Rot Left");
+          transform.Rotate(rotationspeed * Vector3.forward * Time.deltaTime);
         }
       }
       else if (Input.GetKey(KeyCode.D)){
         if (!Input.GetKey(KeyCode.A)) {
-          print("Rot Right");
+          transform.Rotate(-(rotationspeed * Vector3.forward * Time.deltaTime));
         }
       }
+    }
+
+    public static IEnumerator FadeOut (AudioSource ad, float fadetime) {
+      float startVol = ad.volume;
+
+      while (ad.volume > 0) {
+        ad.volume -= startVol * Time.deltaTime / fadetime;
+
+        yield return null;
+      }
+
+      ad.Stop();
+      ad.volume = startVol;
     }
   }
