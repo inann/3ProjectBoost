0d610f0b8fd2b3d44348352211934d4969f5ea8e
inann <inann231@gmail.com>
2020-04-07 19:16:09 -0700

    Up through class 63

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,46 +1,79 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidbody;
     [SerializeField] float rcsThrust = 100f;
     [SerializeField] float mainThrust = 1f;
+    // Audio Clip Fields
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip explosion;
+    [SerializeField] AudioClip winJingle;
+
+    // Particle System Fields
+    [SerializeField] ParticleSystem thrustParticles;
+    [SerializeField] ParticleSystem deathParticles;
+    [SerializeField] ParticleSystem winParticles;
+
     float fadeDuration = 0.75f;
     AudioSource audiosource;
+    public float delayTime = 3f;
+
+    enum State { Alive, Dying, Transcending };
+    State state = State.Alive;
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
       rigidbody = GetComponent<Rigidbody>();
       audiosource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
       ProcessInput();
     }
 
     private void ProcessInput() {
-      Thrust();
-      Rotate();
+      if (state == State.Alive) {
+        Thrust();
+        Rotate();
+      }
     }
 
     private void Thrust() {
-      if (Input.GetKeyDown(KeyCode.Space)) {
-        audiosource.Play();
-      }
-
-      if (Input.GetKeyUp(KeyCode.Space)) {
-        IEnumerator fd = FadeOut(audiosource, fadeDuration);
-        StartCoroutine(fd);
-      }
-
       if (Input.GetKey(KeyCode.Space)) {
         print("Thrust");
         rigidbody.AddRelativeForce(Vector3.up * mainThrust);
+        playThrustSound();
+        thrustParticles.Play();
+      } else {
+        audiosource.Stop();
+        thrustParticles.Stop();
       }
+    }
+
+    private void playThrustSound() {
+      if (!audiosource.isPlaying) {
+        audiosource.PlayOneShot(mainEngine);
+      }
+    }
+
+    private void playWinCondition() {
+      audiosource.Stop();
+      audiosource.PlayOneShot(winJingle);
+      thrustParticles.Stop();
+      winParticles.Play();
+    }
+
+    private void playDeathsplosion() {
+      audiosource.Stop();
+      audiosource.PlayOneShot(explosion);
+      thrustParticles.Stop();
+      deathParticles.Play();
     }
 
     private void Rotate() {
@@ -62,6 +95,9 @@ public class Rocket : MonoBehaviour
     }
 
     void OnCollisionEnter(Collision collision) {
+      if (state != State.Alive) {
+        return;
+      }
       // Check the collision object for tags
       switch (collision.gameObject.tag) {
         case "Friendly":
@@ -69,24 +105,29 @@ public class Rocket : MonoBehaviour
             print("Friendly");
             break;
           }
+        case "Finish":
+          {
+            state = State.Transcending;
+            playWinCondition();
+            Invoke("LoadNextScene", delayTime);
+            break;
+          }
         default:
           {
-            print("Death");
+            state = State.Dying;
+            playDeathsplosion();
+            Invoke("LoadDeathScene", delayTime);
             break;
           }
       }
     }
 
-    public static IEnumerator FadeOut (AudioSource ad, float fadetime) {
-      float startVol = ad.volume;
-
-      while (ad.volume > 0) {
-        ad.volume -= startVol * Time.deltaTime / fadetime;
-
-        yield return null;
-      }
-
-      ad.Stop();
-      ad.volume = startVol;
+    private void LoadDeathScene() {
+      SceneManager.LoadScene(0);
     }
+
+    private void LoadNextScene() {
+      SceneManager.LoadScene(1);
+    }
+
   }
